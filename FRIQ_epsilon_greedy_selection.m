d562bd118e42aa21ea7cee4a9d7b61c1a4dc3d77
szaguldo-kamaz <github@kamm.hu>
2021-10-07 14:52:52 +0900

    initial 0.60

---
+++ FRIQ_epsilon_greedy_selection.m
@@ -0,0 +1,25 @@
+function [ action ] = FRIQ_epsilon_greedy_selection(state, epsilon, actionset)
+% FRIQ_epsilon_greedy_selection: epsilon-greedy strategy action selection
+%
+% FRIQ-learning framework v0.60
+% https://github.com/szaguldo-kamaz/
+%
+% Author: David Vincze <david.vincze@uni-miskolc.hu>
+% Copyright (c) 2013-2021 by David Vincze
+%
+
+    global numofactions
+
+    if (epsilon == 0) % if random actions are disabled - choose the best
+        action = FRIQ_get_best_action(state, actionset);
+    else
+        if (rand() > epsilon)
+            action = FRIQ_get_best_action(state, actionset);
+        else
+            % random action
+            action = floor(rand()*(numofactions))+1;
+            if (action > numofactions)
+                action = numofactions;
+            end
+        end
+    end
