da552755d0c52968ce9f514352bd5ef095725bae
szaguldo-kamaz <github@kamm.hu>
2021-10-08 19:30:16 +0900

    initial 0.60

---
+++ FRIQ_quantize_observations.m
@@ -0,0 +1,25 @@
+function [ state_quantized ] = FRIQ_quantize_observations(state, states, statedivs)
+% FRIQ_quantize_observations: perform quantization of the inputs, use only the allowed state values
+%
+% FRIQ-learning framework v0.60
+% https://github.com/szaguldo-kamaz/
+%
+% Author: David Vincze <david.vincze@uni-miskolc.hu>
+% Copyright (c) 2013-2021 by David Vincze
+%
+
+    global numofstates
+
+    state_quantized = zeros(1, numofstates);
+
+    for stateno = 1:numofstates
+        nosubstates = size(states{stateno}, 2);
+        where = round((state(stateno) + abs(states{stateno}(1))) ./ statedivs{stateno}) + 1;
+        if where < 1
+            where = 1;
+        elseif where > nosubstates
+            where = nosubstates;
+        end
+
+        state_quantized(stateno) = states{stateno}(where);
+    end
